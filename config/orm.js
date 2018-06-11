323e514c44e0508a39f1a9c1abc9ced99a1e9225
prasangimunindradasa <prasangi.m.m@gmail.com>
2018-06-11 04:32:18 -0700

    final

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+import { DH_NOT_SUITABLE_GENERATOR } from 'constants';
+
+var connection = require('./connection.js');
+
+var orm = {
+    all: function(tableInput, cb){
+        connection.query('SELECT * FROM '+tableInput+';', function(err,result){
+            if(err) throw err;
+            cb(result)
+        })
+    },
+
+    update: function(tableInput, condition, cb){
+        connection.query('UPDATE '+tableInput+' SET devoured=true WHERE id='+condition+';',function(err,result){
+            cb(result);
+        })
+    },
+
+create: function(tableInput,val,cb){
+    connection.query('INSERT INTO '+tableInput+" (burger_name) VALUES ('"+val+"');", function(err,result){
+        if(err)throw err;
+        cb(result);
+    })
+}
+}
+
+module.exports = orm;
